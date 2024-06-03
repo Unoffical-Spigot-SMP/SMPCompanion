6ed5e907ab6577ab4d4f1ce9f8420fc7d1419a77
KaspianDev <kacperleaguenine@gmail.com>
2024-06-03 18:01:48 +0200

    local chat 2nd try

--- src/main/java/net/overlordmc/smpcompanion/module/impl/LocalChatModule.java
+++ src/main/java/net/overlordmc/smpcompanion/module/impl/LocalChatModule.java
@@ -4,12 +4,16 @@ import net.overlordmc.smpcompanion.SMPCompanion;
 import net.overlordmc.smpcompanion.module.Module;
 import net.overlordmc.smpcompanion.module.ModuleProperties;
 import net.overlordmc.smpcompanion.module.annotation.PropertyPath;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class LocalChatModule extends Module implements Listener {
 
@@ -48,14 +52,24 @@ public class LocalChatModule extends Module implements Listener {
         Player sender = event.getPlayer();
         World senderWorld = sender.getWorld();
         Location senderLocation = sender.getLocation();
-        event.getRecipients().removeIf((recipient) -> {
-            if (recipient.getWorld() != senderWorld) return true;
 
-            double distance = senderLocation.distanceSquared(recipient.getLocation());
-            System.out.println(distance);
-            System.out.println(Math.pow(maxDistance, 2));
-            return distance >= Math.pow(maxDistance, 2);
-        });
+        Set<Player> localPlayers = event.getRecipients().stream()
+                                        .filter((recipient) -> {
+                                            if (recipient.getWorld() != senderWorld) return false;
+
+                                            double distance = senderLocation.distanceSquared(recipient.getLocation());
+                                            System.out.println(distance);
+                                            System.out.println(Math.pow(maxDistance, 2));
+                                            System.out.println(distance >= Math.pow(maxDistance, 2));
+
+                                            return distance >= Math.pow(maxDistance, 2);
+                                        })
+                                        .collect(Collectors.toUnmodifiableSet());
+
+        event.setCancelled(true);
+
+        AsyncPlayerChatEvent chatEvent = new AsyncPlayerChatEvent(true, sender, event.getMessage(), localPlayers);
+        Bukkit.getPluginManager().callEvent(chatEvent);
     }
 
 }
